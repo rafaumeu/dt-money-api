64f585f32fbedf6652f582d2de8274aff9d23f1f
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-01-15 10:59:28 -0300

    fix(server): update CORS configuration to allow all origins and add authorization header - Update CORS configuration to allow all origins for development purposes - Add authorization header to CORS configuration for future authentication needs

--- api/server.js
+++ api/server.js
@@ -5,16 +5,17 @@ const middlewares = jsonServer.defaults();
 
 // Middleware para configurar CORS
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4000'); // Domínio específico
+  res.header('Access-Control-Allow-Origin', '*'); // Permitir todos os domínios (ajuste para seu caso de uso)
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Incluindo o cabeçalho de autorização, caso necessário
   
   if (req.method === 'OPTIONS') {
-    return res.sendStatus(200); // Responde rápido para OPTIONS
+    return res.sendStatus(200); // Responde para requisições OPTIONS
   }
   
   next();
 });
+
 
 // Usar middlewares padrão do JSON Server
 server.use(middlewares);
